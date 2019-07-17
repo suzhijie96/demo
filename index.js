afe00cda7ded759cdbe41a13f11cf1a9242121ab
suzhijie96 <1291231803@qq.com>
2019-07-17 11:28:23 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+var app = new Vue({
+    el:'#app',
+    data: {
+        message:'Hello Vue!'
+    }
+});
+
+app.message = 'I have changed the data!';
+
+var app2 = new Vue({
+    el:'#app_2',
+    data:{
+        message:'页面加载于' + new Date().toLocaleString(),
+        baidu:'www.baidu.com',
+        text:'百度链接',
+        isTrue:false,
+        isActive:false,
+        bool:'对象语法',
+    }
+});
